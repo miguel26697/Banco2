776d7baa7b107a965386d239b95ce7aa087c71e5
Miguel Rippe <miguelrippe01@gmail.com>
2020-08-25 20:12:48 -0500

    Creacion de clase producto

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,2 +1,30 @@
 public class Cliente {
+
+    String Nombre;
+    String Apellidos;
+    int Cedula;
+    String Direccion;
+
+
+    public Cliente(String nombre, String apellidos, int cedula, String direccion) {
+        Nombre = nombre;
+        Apellidos = apellidos;
+        Cedula = cedula;
+        Direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "Nombre='" + Nombre + '\'' +
+                ", Apellidos='" + Apellidos + '\'' +
+                ", Cedula=" + Cedula +
+                ", Direccion='" + Direccion + '\'' +
+                '}';
+    }
+
+
+
+
+
 }
